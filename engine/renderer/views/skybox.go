8de1988eee50ba9af907712fc810abb3ae4d4d55
Davide Berdin <davideberdin@gmail.com>
2024-10-17 23:16:03 +0200

    Add renderview system

--- engine/renderer/views/skybox.go
+++ engine/renderer/views/skybox.go
@@ -1,1 +1,25 @@
 package views
+
+import "github.com/spaghettifunk/anima/engine/renderer/metadata"
+
+func RenderViewSkyboxOnCreate() bool {
+	return false
+}
+
+func RenderViewSkyboxOnDestroy() error {
+	return nil
+}
+
+func RenderViewSkyboxOnResize(width, height uint32) {}
+
+func RenderViewSkyboxOnBuildPacket(data interface{}) (*metadata.RenderViewPacket, error) {
+	return nil, nil
+}
+
+func RenderViewSkyboxOnDestroyPacket(packet *metadata.RenderViewPacket) {
+
+}
+
+func RenderViewSkyboxOnRender(packet *metadata.RenderViewPacket, frame_number, render_target_index uint64) bool {
+	return false
+}
--- engine/renderer/views/world.go
+++ engine/renderer/views/world.go
@@ -1,1 +1,25 @@
 package views
+
+import "github.com/spaghettifunk/anima/engine/renderer/metadata"
+
+func RenderViewWorldOnCreate() bool {
+	return false
+}
+
+func RenderViewWorldOnDestroy() error {
+	return nil
+}
+
+func RenderViewWorldOnResize(width, height uint32) {}
+
+func RenderViewWorldOnBuildPacket(data interface{}) (*metadata.RenderViewPacket, error) {
+	return nil, nil
+}
+
+func RenderViewWorldOnDestroyPacket(packet *metadata.RenderViewPacket) {
+
+}
+
+func RenderViewWorldOnRender(packet *metadata.RenderViewPacket, frame_number, render_target_index uint64) bool {
+	return false
+}
